f4a1d541f02c76fcfd791a6fc4a13fd4e8e79763
SwiftyWang <swifty.wang@tinklabs.com>
2017-03-20 10:38:13 +0800

    fine tune.

--- library/src/main/java/com/swifty/datareader/v23/V23DataReader.java
+++ library/src/main/java/com/swifty/datareader/v23/V23DataReader.java
@@ -227,6 +227,7 @@ public class V23DataReader implements IReader {
                 appNetData.tp += bucket.getTxPackets();
             }
         }
+        if (networkStats != null) networkStats.close();
         return appNetDataMap;
     }
 
